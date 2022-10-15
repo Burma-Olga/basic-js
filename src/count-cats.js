a98da7ea33f118877f94c5650a673a733abe3ca2
Vadim_Sedelnikov <Burma_Olga@bk.ru>
2022-10-15 14:34:33 +0300

    fix: change count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -17,13 +17,13 @@ const { NotImplementedError } = require('../extensions/index.js');
 function countCats(matrix) {
   count = 0;
   for(i=0; i<matrix.length; i++){
-    for(j=0; j<matrix.length; j++){ 
+    for(j=0; j<matrix[i].length; j++){ 
     if((matrix[i][j]) === '^^'){
       count++;
     } 
   }
-  return count;
 }
+return count;
 }
 
 module.exports = {
