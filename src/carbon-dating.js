ce7126a9697d55016a9eb5301a493b95ed55c661
Vadim_Sedelnikov <Burma_Olga@bk.ru>
2022-10-15 14:09:41 +0300

    feat: task carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+  if (!sampleActivity) { return false }
+  else if (Number.isNaN(Number(sampleActivity))) {
+    return false
+  } else if (Number(sampleActivity) > 15 || Number(sampleActivity) <= 0) {
+    return false
+  } else if (typeof sampleActivity !== 'string') {
+    return false
+  }
+
+  const firstOrder = Math.log(MODERN_ACTIVITY / sampleActivity)
+  const k = 0.693 / HALF_LIFE_PERIOD
+  return Math.ceil(firstOrder / k);
 }
 
 module.exports = {
